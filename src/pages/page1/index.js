d641d5552c6ebd09045ec8badcfc6b8fb9ed879e
Lucas Koval <lucaskovaldev@gmail.com>
2021-11-09 22:50:12 -0300

    First commit

---
+++ src/pages/page1/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MainSection, PageContainer, Subtitle } from '../../styles/globalStyles'
+
+export default function Page1() {
+  return (
+    <MainSection className="MainSection">
+      <Subtitle>Subtitle</Subtitle>
+      <PageContainer className="PageContainer">
+        <h1>Page 1</h1>
+      </PageContainer>
+    </MainSection>
+  )
+}
